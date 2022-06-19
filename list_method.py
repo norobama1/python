282f285542f93da3ec15ba3bcc4544ea884c426e
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-19 19:25:10 +0530

    Create list_method.py

---
+++ list_method.py
@@ -0,0 +1,36 @@
+N1 = [10,5,77,11,30,3]
+
+#sorting of list
+N1.sort()
+print(N1)
+
+L1 = [3,77,11,9,10,22,3]
+
+#reverse of list
+L1.reverse()
+print(L1)
+
+#append the list (adding element at the end)
+L1.append(100)
+print(L1)
+
+#insert element at any postion
+L1.insert(3,8)
+print(L1)
+
+
+#pop  (will remove the element at given index and return its value )
+L1.pop(3)
+print(L1)
+
+#remove (will remove the element from the list)
+L1.remove(11)
+print(L1)
+
+#clear (remove all elements from the list)
+N1.clear()
+print(N1)
+
+#count 
+print(L1.count(3))
+
