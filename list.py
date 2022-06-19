67f0de2e4c6c364a4bde7e7d1080f56c82e2b66b
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-19 19:24:32 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+# create a list using []
+a = [1,2,5,7,8]
+
+#print the list using print() function
+print(a)
+
+#Access using index using a[0],a[1],a[2]......
+print(a[2])
+
+#change the value of list
+a[0] = 30
+print(a)
+
+#we can create list with different datatypes
+c =[20, "Naman",False,9.0]
+print(c)
+
+#list slicing
+n = ["Naman","Noro","Ceci","Rajat","Tallen","hituu"]
+print(n[0:2])
