dbeb5141c0bbe36b7bf61bd82cbabd4df70e1847
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-19 19:25:44 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+# create a tuple using ()
+t = (1,3,5,7,10)
+
+t1 = () #Empty Tuple
+t2 = (2,) #Tuple with single element
+
+
+#printing the elements of the tuplr
+print(t)
+
+#Access using index using t[0],t[1],t[2]......
+print(t[2])
+
+#cannot update the values of tuple
+t[0] = 34
