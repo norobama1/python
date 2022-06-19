b8e9142c7c215db6296cc88ff0b42bf91047d45f
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-19 19:26:55 +0530

    Create list_sortmarks.py

---
+++ list_sortmarks.py
@@ -0,0 +1,11 @@
+#store the marks of six student and sort them 
+m1 = int(input("Enter the marks of Student 1 :"))
+m2 = int(input("Enter the marks of Student 2 :"))
+m3 = int(input("Enter the marks of Student 3 :"))
+m4 = int(input("Enter the marks of Student 4 :"))
+m5 = int(input("Enter the marks of Student 5 :"))
+m6 = int(input("Enter the marks of Student 6 :"))
+
+Mylist = [m1,m2,m3,m4,m5,m6]
+Mylist.sort()
+print(Mylist)
