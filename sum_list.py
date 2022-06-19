1775abc84a5e97ffe81467287d38db78f21a04bd
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-19 19:27:51 +0530

    Create sum_list.py

---
+++ sum_list.py
@@ -0,0 +1,8 @@
+#find the sum of four numbers
+s1 = int(input("Enter Number 1 :"))
+s2 = int(input("Enter Number 2 :"))
+s3 = int(input("Enter Number 3 :"))
+s4 = int(input("Enter Number 4 :"))
+
+mysum = [s1,s2,s3,s4]
+print(sum(mysum))
