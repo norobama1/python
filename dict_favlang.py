1030a7a60af54719bc29521f98bafaf2ea38d24d
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-06-24 13:10:07 +0530

    Create dict_favlang.py

---
+++ dict_favlang.py
@@ -0,0 +1,33 @@
+#create an empty dict,allow 4 friends to enter their fav lang as values and use key as name and names are unqiue
+# FavLang = {}
+
+# a = input("Enter your favourite language Naman: ")
+# b = input("Enter your favourite language Noro: ")
+# c = input("Enter your favourite language Hits: ")
+# d = input("Enter your favourite language Norobam: ")
+
+# FavLang['Naman'] = a
+# FavLang['Noro'] = b
+# FavLang['Hits'] = c
+# FavLang['Norobama'] = d
+
+# print(FavLang)
+
+
+#create an empty dict,allow 4 friends to enter their fav lang as values and use key as name and names are unqiue
+
+FavLang = {}
+
+a = input("Enter your favourite language Naman: ")
+b = input("Enter your favourite language Noro: ")
+c = input("Enter your favourite language Naman: ")
+d = input("Enter your favourite language Norobam: ")
+
+FavLang['Naman'] = a
+FavLang['Noro'] = b
+FavLang['Naman'] = c
+FavLang['Norobama'] = d
+
+print(FavLang)
+
+#when same name had different values than the latter one is chosen
