4aa25b89cae2c4203c1533ff161404f5520bbbeb
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-07-01 16:12:00 +0530

    Create relational_operators.py

---
+++ relational_operators.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age"))
+
+if(a>40 and a<69):
+    print("You can work with us")
+else:
+        print("You cant work with us")
+
+if(a>40 or a<69):
+    print("You can work with us")
+else:
+        print("You cant work with us")
