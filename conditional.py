d12942c0c8f5da1a6dfe838c744ffde2ad2f6349
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-07-01 16:11:23 +0530

    Create conditional.py

---
+++ conditional.py
@@ -0,0 +1,29 @@
+a = 15
+
+#if-elif-else ladder
+# if(a<3):
+#     print("The value of a is less then 3")
+# elif(a>13):
+#     print("The value of a is greater then 13")
+# elif(a>7):
+#     print("The value of a is greater then 7")
+# else:
+#     print("None of the above")
+
+# print("done")
+
+
+
+#multiple if statement
+if(a<3):
+    print("The value of a is less then 3")
+if(a>11):
+    print("The value of a is greater then 11")
+if(a>13):
+    print("The value of a is greater then 13")
+if(a>20):
+    print("The value of a is greater then 7")
+else:
+    print("None of the above")
+
+print("done")
