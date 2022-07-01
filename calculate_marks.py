36f31146b805be5e2ee24ee06d176f579fd1a166
norobama1 <98684707+norobama1@users.noreply.github.com>
2022-07-01 16:13:44 +0530

    Add files via upload

---
+++ calculate_marks.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter your marks: "))
+
+if(100>=marks>=91):
+    print("Your grade is Ex")
+elif(90>=marks>=81):
+    print("Your grade is A")
+elif(80>=marks>=71):
+    print("Your grade is B")
+elif(70>=marks>=61):
+    print("Your grade is C")    
+elif(60>=marks>=51):
+    print("Your grade is D")
+elif(50>=marks>=41):
+    print("Your grade is E")        
+else:
+    print("Fail")    
